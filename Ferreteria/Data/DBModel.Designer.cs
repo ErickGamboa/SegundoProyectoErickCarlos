57b7620bf81b568f1c39acbb0e86de396edc4014
CARLOS MARIO JIMENEZ BARRANTES <cjimenezb@est.utn.ac.cr>
2020-11-16 20:32:54 -0600

    Se realizaron los reportes 2, 8 y 9

--- Ferreteria/Data/DBModel.Designer.cs
+++ Ferreteria/Data/DBModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\UTN\OneDrive - Universidad Técnica Nacional\2020-IIIC\Programación III\Repositorio\SegundoProyectoErickCarlos\Ferreteria\Data\DbModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\UTN\OneDrive - Universidad Técnica Nacional\2020-IIIC\Programación III\Repositorio\SegundoProyectoErickCarlos\Ferreteria\Data\DBModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
---
+++ Ferreteria/Entities/Reportes/Reporte9E.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Reportes
+{
+    public class Reporte9E
+    {
+        public string CodigoUsuario { get; set; }
+        public int RealizoVentas { get; set; }
+
+        public Reporte9E() { }
+
+        public Reporte9E(string codigoUsuario, int realizoVentas)
+        {
+            CodigoUsuario = codigoUsuario;
+            RealizoVentas = realizoVentas;
+        }
+    }
+}
